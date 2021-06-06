21db69c8008c918ff48073e8aec287ab1976e9e1
송병선 <byung0216@gmail.com>
2021-06-06 09:36:16 +0900

    플레이어 이미지 및 코드 작성
    1. 플레이어 이미지 다운
    2. PlayerState 코드 작성 중

--- Assets/Scripts/Player/PlayerStates/PlayerState.cs
+++ Assets/Scripts/Player/PlayerStates/PlayerState.cs
@@ -8,15 +8,45 @@ public class PlayerState
     protected PlayerStateMachine stateMachine;
     protected PlayerData playerData;
 
-    protected float stateTime;
+    protected float startTime;
 
     private string animBoolName;
 
+    //생성자
     public PlayerState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName)
     {
         this.player = player;
         this.stateMachine = stateMachine;
         this.playerData = playerData;
         this.animBoolName = animBoolName;
+    }
+
+    //해당 State가 시작될 때 한 번 실행
+    public virtual void Enter()
+    {
+        DoChecks();
+        player.Anim.SetBool(animBoolName, true);
+        startTime = Time.time;
+    }
+
+    //해당 State가 끝날 때 한 번 실행
+    public virtual void Exit()
+    {
+        player.Anim.SetBool(animBoolName, false);
+    }
+
+    public virtual void LogicUpdate()
+    {
+
+    }
+
+    public virtual void PhysicsUpdate()
+    {
+
+    }
+
+    public virtual void DoChecks()
+    {
+
     }
 }
