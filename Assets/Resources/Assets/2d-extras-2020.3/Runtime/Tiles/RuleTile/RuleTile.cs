a313e23c86679d68897c9e9c6bb231f1f0b196b1
송병선 <byung0216@gmail.com>
2021-07-03 19:33:34 +0900

    룰 타일 추가 및 타일 파레트 수정 중

--- Assets/Resources/Assets/2d-extras-2020.3/Runtime/Tiles/RuleTile/RuleTile.cs
+++ Assets/Resources/Assets/2d-extras-2020.3/Runtime/Tiles/RuleTile/RuleTile.cs
@@ -24,7 +24,7 @@ namespace UnityEngine
     /// Generic visual tile for creating different tilesets like terrain, pipeline, random or animated tiles.
     /// </summary>
     [Serializable]
-    [HelpURL("https://docs.unity3d.com/Packages/com.unity.2d.tilemap.extras@latest/index.html?subfolder=/manual/RuleTile.html")]
+    [CreateAssetMenu]
     public class RuleTile : TileBase
     {
         /// <summary>
